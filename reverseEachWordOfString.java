53f7f4c6af33cd4ee3a0a80ba83ada730559c6c0
Nirmala <nirmala8579@gmail.com>
2025-09-21 11:59:08 +0530

    reverseEachWordOfString.java

---
+++ reverseEachWordOfString.java
@@ -0,0 +1,22 @@
+public static void main(String[] args) {
+reverseEachWordOfString("Java is good programming langauges");
+}
+static void reverseEachWordOfString(String inputString)
+{
+String[] words = inputString.split(" ");
+String reverseString = "";
+for (int i = 0; i < words.length; i++) {
+String word = words[i];
+String nstr = "";
+char ch;
+for (int j = 0; j < word.length(); j++) {
+ch = word.charAt(j);
+nstr = ch + nstr;
+}
+reverseString = reverseString + nstr + " ";
+}
+System.out.println(inputString);
+System.out.println(reverseString);
+}
+Input: Java is good programming langauges
+Output: avaJ si doog gnimmargorp seguagnal
