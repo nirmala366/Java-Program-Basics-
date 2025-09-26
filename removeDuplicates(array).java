dd4fb2402b2781930152e5e493081cb8c3d598a1
Nirmala <nirmala8579@gmail.com>
2025-09-26 18:05:53 +0530

    removeDuplicates(array).java

---
+++ removeDuplicates(array).java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+public class Main {
+public static void main(String[] args) {
+int[] array = {5, 2, 9, 1, 6, 2, 5};
+int[] uniqueArray = removeDuplicates(array);
+System.out.println("Array with duplicates removed:");
+for (int num : uniqueArray) {
+System.out.print(num + " ");
+}
+}
+public static int[] removeDuplicates(int[] array) {
+Set<Integer> set = new HashSet<>();
+for (int num : array) {
+set.add(num);
+}
+int[] result = new int[set.size()];
+int i = 0;
+for (int num : set) {
+result[i++] = num;
+}
+return result;
+}
+}
